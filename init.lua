4554af777e730dcd7787b359aad9bad58bf77558
root <root@s034-adm0018.s034.oc.equinor.com>
2024-03-14 10:40:57 +0100

    added relative line numbers

---
+++ init.lua
@@ -0,0 +1,4 @@
+require("hasu.core.options")
+require("hasu.core.keymaps")
+require("hasu.core.colorscheme")
+
---
+++ lua/hasu/core/options.lua
@@ -0,0 +1,6 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
